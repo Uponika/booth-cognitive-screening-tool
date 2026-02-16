a7824a66fa70a15449d047abc3d6421b97ce92db
Saikat Datta <47591291+sakatdata@users.noreply.github.com>
2026-02-16 16:52:29 -0500

    Update app.py
    Removed debug=true to deploy in render

--- Application/app.py
+++ Application/app.py
@@ -50,4 +50,5 @@ def serve_assets(filename):
     return send_from_directory("assets", filename)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", port=5000)
+
